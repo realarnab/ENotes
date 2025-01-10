a0d7c5718f838970b5606864fa0f243b5ff6249b
Arnab M. <118125247+realarnab@users.noreply.github.com>
2025-01-10 14:12:29 +0530

    get & delete category implemented
    Create custom method for delete and get category.

--- src/main/java/com/enotes/repository/CategoryRepository.java
+++ src/main/java/com/enotes/repository/CategoryRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    List<Category> findByIsActiveTrue();
+    List<Category> findByIsActiveTrueAndIsDeletedFalse();
+
+    Optional<Category> findByIdAndIsDeletedFalse(Integer id);
+
+    List<Category> findByIsDeletedFalse();
 }
